194e7a3b25da969d8d78767f0f51cc9868166b77
ojiah barnabas ogirima <ogisojiah@gmail.com>
2024-07-17 14:20:25 +0100

    initial Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let bulb =document.querySelector("img")
+function offbulb() {
+    bulb.setAttribute("src","res/offbulb.jpg")
+}
+function onbulb() {
+    bulb.setAttribute("src","res/onbulb.jpg")
+}
